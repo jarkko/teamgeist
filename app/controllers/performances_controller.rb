8578740abfe49f6d410fd7fc2e071ae8c3e481ad
Jarkko Laine <jarkko@jlaine.net>
2009-03-28 18:24:05 +0200

    Added performances#update

--- app/controllers/performances_controller.rb
+++ app/controllers/performances_controller.rb
@@ -1,5 +1,6 @@
 class PerformancesController < ApplicationController
   before_filter :require_user
+  before_filter :fetch_user, :only => [:show, :update]
   
   def index
     @performances = current_user.performances
@@ -11,6 +12,23 @@ class PerformancesController < ApplicationController
   end
   
   def show
+  end
+  
+  def update
+    @performance.update_attributes!(params[:performance])
+    
+    respond_to do |format|
+      format.js do
+        render :text => ""
+      end
+    end
+  end
+  
+  private
+  
+  def fetch_user
     @performance = current_user.performances.find(params[:id])
+  rescue
+    render :text => "Not found", :status => :not_found
   end
 end
