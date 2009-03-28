bc7b08727f701020184393434e2115672156dcd8
Jarkko Laine <jarkko@jlaine.net>
2009-03-28 15:57:53 +0200

    Implemented the users controller and added a route for it.

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,7 @@ ActionController::Routing::Routes.draw do |map|
   # config/routes.rb
   map.resource :user_session
   map.root :controller => "user_sessions", :action => "new"
+  
+  map.resource :account, :controller => "users"
+  map.resources :users
 end
