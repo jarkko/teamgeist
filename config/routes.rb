58e13f428da1cbe50e333e59c042b0b1d50e7c55
Jarkko Laine <jarkko@jlaine.net>
2009-03-28 17:47:32 +0200

    Added a controller for performances.

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 ActionController::Routing::Routes.draw do |map|
-  map.resources :users
+  map.resources :users, :performances
 
   # config/routes.rb
   map.resource :user_session
   map.root :controller => "user_sessions", :action => "new"
-  
+    
   map.resource :account, :controller => "users"
-  map.resources :users
 end
---
+++ test/unit/helpers/performances_helper_test.rb
@@ -0,0 +1,4 @@
+require 'test_helper'
+
+class PerformancesHelperTest < ActionView::TestCase
+end
