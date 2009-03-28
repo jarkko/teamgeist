520c1da5776a1eb4301050d81db87b9c21cc8c50
Jarkko Laine <jarkko@jlaine.net>
2009-03-28 16:41:58 +0200

    Updated user model and validations and tweaked the views.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ActiveRecord::Base
   acts_as_authentic
+  
+  validates_uniqueness_of :login, :email
 end
--- db/migrate/20090328134958_create_users.rb
+++ db/migrate/20090328134958_create_users.rb
@@ -1,19 +1,20 @@
 class CreateUsers < ActiveRecord::Migration
   def self.up
     create_table :users do |t|
-      t.string :login
-      t.string :crypted_password
-      t.string :password_salt
-      t.string :persistence_token
-      t.integer :login_count
-      t.datetime :last_request_at
-      t.datetime :last_login_at
-      t.datetime :current_login_at
-      t.string :last_login_ip
-      t.string :current_login_ip
-      t.string :email
-      t.string :single_access_token
-      t.string :perishable_token
+      t.string    :login,               :null => false
+      t.string    :email,               :null => false
+      t.string    :crypted_password,    :null => false
+      t.string    :password_salt,       :null => false
+      t.string    :persistence_token,   :null => false
+      t.string    :single_access_token, :null => false
+      t.string    :perishable_token,    :null => false
+      t.integer   :login_count,         :null => false, :default => 0
+      t.integer   :failed_login_count,  :null => false, :default => 0
+      t.datetime  :last_request_at
+      t.datetime  :last_login_at
+      t.datetime  :current_login_at
+      t.string    :last_login_ip
+      t.string    :current_login_ip
 
       t.timestamps
     end
