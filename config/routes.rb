d1603963b548dcb158b907dec8c2058ad4716004
Jarkko Laine <jarkko@jlaine.net>
2009-03-28 15:50:52 +0200

    Added user model and users controller for auth_logic

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 ActionController::Routing::Routes.draw do |map|
+  map.resources :users
+
   # config/routes.rb
   map.resource :user_session
   map.root :controller => "user_sessions", :action => "new"
